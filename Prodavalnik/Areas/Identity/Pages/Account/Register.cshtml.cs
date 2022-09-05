edf686e7253f943f87d967d7a9a6f58625b1b53a
marindimitrov12 <63950527+marindimitrov12@users.noreply.github.com>
2022-09-05 18:33:21 +0300

    Adding extra features to the default identity

--- Prodavalnik/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Prodavalnik/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -76,10 +76,16 @@ namespace Prodavalnik.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
+            [Display(Name = "FirstName")]
+            public string FirstName { get; set; }
+            [Required]
+            [Display(Name = "LastName")]
+            public string LastName { get; set; }
+            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
-
+            
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
@@ -114,7 +120,8 @@ namespace Prodavalnik.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
-
+                user.FirstName = Input.FirstName;
+                user.LastName = Input.LastName;
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
